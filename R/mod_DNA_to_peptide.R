8c0975375fdb56c6ead65fed73b6997585e22df3
margretheblyso <s224180@dtu.dk>
2023-10-31 11:01:19 +0000

    last commit in class - error in the app

--- R/mod_DNA_to_peptide.R
+++ R/mod_DNA_to_peptide.R
@@ -11,13 +11,7 @@ mod_DNA_to_peptide_ui <- function(id){
   ns <- NS(id)
   tagList(
     fluidRow(
-      column(8, shiny::textAreaInput(
-        inputId = ns("DNAseq"),
-        label = "DNA sequence",
-        width = 800,
-        height = 100,
-        placeholder = "Insert DNA sequence"
-      )),
+      column(8, shiny::uiOutput(ns("DNAseq"))),
       column(4, shiny::numericInput(
           inputId = ns("randomDNA"),
           label = "Random DNA length",
@@ -25,9 +19,12 @@ mod_DNA_to_peptide_ui <- function(id){
           min = 3,
           max = 100000,
           step = 30)
-          )
-    ),
-    shiny::verbatimTextOutput(outputId = ns("peptide")) |> 
+          ,
+    shiny::actionButton(
+      inputId = ns("generate_dna"),
+      label = "Generate random DNA", style = "margin-top: 18px;"
+    ))),
+    shiny::verbatimTextOutput(outputId = ns("peptide")) |>
       shiny::tagAppendAttributes(style = "white-space: pre-wrap;")
   )
 }
@@ -35,10 +32,40 @@ mod_DNA_to_peptide_ui <- function(id){
 #' DNA_to_peptide Server Functions
 #'
 #' @noRd
+#' @import biocd
 mod_DNA_to_peptide_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
-
+    dna <- reactiveVal()
+    output$DNAseq <- renderUI({
+      textAreaInput(
+        inputId = ns("DNAseq"),
+        label = "DNA sequence",
+        placeholder = "Insert DNA sequence",
+        value = dna(),
+        height = 100,
+        width = 600
+      )
+      observeEvent(input$generate_DNA, {
+        dna(
+          biocd::create_DNA(input$randomDNA)
+          )
+      })
+    })
+    output$peptide <- renderText({
+      # Ensure input is not NULL and is longer than 2 characters
+      if(is.null(input$DNAseq)){
+        NULL
+      } else if(nchar(input$DNAseq) < 3){
+        NULL
+      } else{
+        input$DNAseq |>
+          toupper() |>
+          biocd::DNA_to_RNA() |>
+          biocd::codons_extraction() |>
+          biocd::aa_translation()
+      }
+    })
   })
 }
 
